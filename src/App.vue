f0872e51981b8e0079ba755cdea19ba8fd154e3a
Renan Paixão <renanlemos21@gmail.com>
2022-01-29 19:36:35 -0300

    feat: add top menu

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,30 @@
 <template>
-  <div/>
+  <div class="container">
+    <Header/>
+    <Menu/>
+    <div class="list">
+      <PokemonCard/>
+      <PokemonCard/>
+      <PokemonCard/>
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
+  import Header from './components/Header.vue'
+  import PokemonCard from './components/PokemonCard.vue'
+  import Menu from './components/Menu.vue'
 </script>
 
-<style>
+<style lang="scss" scoped>
+  .container{
+    min-height: 100vh;
+    background-color: $background-gray;
+    .list{
+      margin-top: 5rem;
+      display: flex;
+      justify-content: space-evenly;
+
+    }
+  }
 </style>
---
+++ src/components/Menu.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h2> <img alt="Profile" src="../assets/group-avatar.png"> Hello {{username}}!!</h2>
+    <p>My Teams</p>
+    <p>Pokedex</p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import { ref } from 'vue'
+
+  const username = ref('Everton')
+</script>
+
+<style lang="scss" scoped>
+  div{
+    display: flex;
+    padding: 1rem 0;
+
+    white-space: nowrap;
+    background-color: $white;
+    color: $title-black;
+
+
+    h2{
+      display: flex;
+      font-family: $roboto;
+      font-weight: 500;
+      font-size: 2.25rem;
+      margin: 0 auto 0 3rem;
+
+      img{
+        margin-right: 0.5rem;
+      }
+
+    }
+    p{
+      font-family: $play;
+      margin: 0 2rem 0 1rem;
+      font-size: 2.25rem;
+      font-weight: lighter;
+    }
+  }
+</style>

399d4ea91921f57a3c8d5306300a3ba529399435
Renan Paixão <renanlemos21@gmail.com>
2022-01-30 01:11:51 -0300

    feat: add component to add a new pokemon

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="container">
+  <div class="default-wrapper">
     <Header/>
     <Menu/>
+    <NavigationMenu teamName="Team Super Aquatic"/>
     <div class="list">
       <PokemonCard/>
       <PokemonCard/>
       <PokemonCard/>
+      <AddPokemon/>
     </div>
   </div>
 </template>
@@ -14,17 +16,18 @@
   import Header from './components/Header.vue'
   import PokemonCard from './components/PokemonCard.vue'
   import Menu from './components/Menu.vue'
+  import AddPokemon from './components/AddPokemon.vue'
+  import NavigationMenu from './components/NavigationMenu.vue'
 </script>
 
 <style lang="scss" scoped>
-  .container{
+  .default-wrapper{
     min-height: 100vh;
     background-color: $background-gray;
     .list{
-      margin-top: 5rem;
+      margin: 5rem 3rem 0;
       display: flex;
       justify-content: space-evenly;
-
     }
   }
 </style>
---
+++ src/components/AddPokemon.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <img src="../assets/plus.png" alt="add pokemon">
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style lang="scss" scoped>
+  div{
+    width: 11.875rem;
+    height: 13rem;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    border: $secondary-gray dashed 2px;
+    border-radius: 26px;
+    cursor: pointer;
+  }
+
+</style>

5b050014ee913367a7fb3d268e1d699fc8695676
Renan Paixão <renanlemos21@gmail.com>
2022-01-30 02:21:43 -0300

    feat: create team list item component

---
+++ src/components/TeamItem.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="container">
+    <span/>
+    <h3>{{teamName}}</h3>
+    <div class="actionsButtons">
+      <img src="src/assets/edit.png" alt="edit time">
+      <img src="src/assets/red-remove.png" alt="delete time">
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  const props = defineProps<{
+    teamName: string
+  }>()
+</script>
+
+<style lang="scss" scoped>
+  .container{
+    height: 3.75rem;
+    font-size: 1.2rem;
+
+    display: flex;
+    align-items: center;
+    padding: 0 0.75rem;
+    box-shadow: 0 4px 10px rgba(0, 0, 0, 0.25);
+    border-radius: 10px;
+    font-family: $russo;
+
+    background-color: $white;
+    span{
+      width: 2px;
+      height: 2.5rem;
+
+      background-color: $primary;
+      margin-right: 0.5rem;
+      border-radius: 10%;
+    }
+    .actionsButtons{
+      margin: 0 2rem 0 auto;
+      img{
+        height: 1.75rem;
+
+        margin: 0 1rem;
+        cursor: pointer;
+      }
+    }
+  }
+
+</style>

bb3572814c3e87b24726ee08b1eb627f7dfcea2e
Renan Paixão <renanlemos21@gmail.com>
2022-02-05 13:27:52 -0300

    feat: add axios and create a state inteface

---
+++ src/interfaces/State.ts
@@ -0,0 +1,6 @@
+export default interface State{
+    pokemons: object[],
+    teamsList: object[],
+    selectToShowTeam: number | null,
+    selectToEditTeam: number | null
+}
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,17 +1,34 @@
-import { createStore } from 'vuex'
+import {createStore} from 'vuex'
+import State from '../interfaces/State'
+import ditto from './tempPoke'
 
 export default createStore({
-    state(){
+    state():State {
         return {
-            pokemons: [],
-            teamsList: []
+            pokemons: [ditto],
+            teamsList: [
+                {id: 1, teamName: 'Team Super Aquatic', pokemonsList: [ditto, ditto, ditto]},
+                {id: 2, teamName: 'Team Galaxy'},
+                {id: 3, teamName: 'The Super Team'}
+            ],
+            selectToShowTeam: null,
+            selectToEditTeam: null,
         }
     },
-    getters:{},
-    actions: {},
+    getters: {},
+    actions: {
+        editTeam(context){
+            console.log(context)
+            context.commit('selectTeamToShow')
+            context.commit('selectTeamToEdit')
+        }
+    },
     mutations: {
-        addTeam(state: any, team:string){
-            state.teamsList.push(team)
+        selectTeamToShow(state:State, id:number){
+            state.selectToShowTeam = id
+        },
+        selectTeamToEdit(state:State, id:number){
+            state.selectToEditTeam = id
         }
     }
 })
